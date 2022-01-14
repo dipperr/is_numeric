399e461b5e7d166038442fc6c9a880929e92b506
dipperr <38946313+dipperr@users.noreply.github.com>
2022-01-14 17:19:38 -0400

    Create is_numeric.py

---
+++ is_numeric.py
@@ -0,0 +1,21 @@
+def is_numeric(num):
+    if num.find(',') != -1:
+        lista_bool = []
+        lista_nums = num.split(',')
+        for i in lista_nums:
+            if i.isnumeric():
+                lista_bool.append(True)
+            else:
+                lista_bool.append(False)
+        return all(lista_bool)
+    elif num.find('.') != -1:
+        lista_bool = []
+        lista_nums = num.split('.')
+        for i in lista_nums:
+            if i.isnumeric():
+                lista_bool.append(True)
+            else:
+                lista_bool.append(False)
+        return all(lista_bool)
+    else:
+        return num.isnumeric()
